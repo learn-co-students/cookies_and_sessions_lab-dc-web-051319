01af9ddde20b12520653dc5dc1cd7a5849e8971b
newtypeV2 <gianlibunao@gmail.com>
2019-06-15 15:14:17 -0400

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'products#index'
+  post '/' => 'products#add'
+  post "", to: "products#index", as: "cart"
+  resources :test
+    # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
