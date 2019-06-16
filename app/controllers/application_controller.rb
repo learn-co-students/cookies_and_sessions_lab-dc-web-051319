888956ebd49503160c1f214241ef748b179ad99f
Nicholas Orochena <nicholas.orochena@gmail.com>
2019-06-15 22:22:34 -0400

    Completed lab

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,16 @@
+# frozen_string_literal: true
+
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  helper_method :cart
+
+  def home
+    render :home
+  end
+
+  def cart
+    session[:cart] ||= []
+  end
 end
