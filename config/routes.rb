b3b645936c0097049da3a0c6003fe375c71ecbe8
Jennifer Ingram <jennifer.ingram@flatironschool.com>
2019-06-17 17:59:57 -0400

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '/', to: 'application#show_form'
+  post '/add_to_cart', to: 'products#add_to_cart'
+  resources 'products', only: [:index]
 end
